f15d6e6d612e8239140d00a3c139334543e5deb8
Paloma Jimenez <r01010010@gmail.com>
2020-03-25 02:01:46 +0100

    Https support

--- src/network/main_server/routes.js
+++ src/network/main_server/routes.js
@@ -2,7 +2,7 @@ const hostname = window && window.location && window.location.hostname;
 const port = (hostname === 'localhost') ? '8080' : '';
 
 const routes = {
-  basepath: `http://${hostname}:${port}/api/`,
+  basepath: `http${hostname === 'localhost' ? '' : 's'}://${hostname}:${port}/api/`,
   users: {
     create: 'users'
   }
