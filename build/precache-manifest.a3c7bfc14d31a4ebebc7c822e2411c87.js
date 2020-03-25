5b2474cb6ec0e258faf70582c574741aad490afc
Paloma Jimenez <r01010010@gmail.com>
2020-03-25 02:11:06 +0100

    je

---
+++ build/precache-manifest.a3c7bfc14d31a4ebebc7c822e2411c87.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea40db84b1d02ee6e783",
+    "url": "/static/js/main.ea40db84.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "08bbb806bddae0661d1d",
+    "url": "/static/js/2.08bbb806.chunk.js"
+  },
+  {
+    "revision": "efa8245dfbcca479f00484311c9b0538",
+    "url": "/index.html"
+  }
+];
